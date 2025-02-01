d08fd33b381467fedcf95b46d0a73469ff8f34c6
umbracious <63139410+umbracious@users.noreply.github.com>
2025-02-01 14:29:01 -0500

    added stripe skeleton

--- genzineers/src/components/navbar.tsx
+++ genzineers/src/components/navbar.tsx
@@ -5,7 +5,7 @@ import SignIn from "@/components/signIn";
 
 export default function Navbar () {
     return (
-        <nav className="bg-black w-full fixed top-0 left-0 right-0 z-50 max-md:hidden">
+        <nav className="bg-black w-full sticky top-0 left-0 right-0 z-50 max-md:hidden">
             <div className="text-white w-full flex flex-nowrap items-center justify-between px-8 py-2">
                 <div>
                     <Link href="/">
