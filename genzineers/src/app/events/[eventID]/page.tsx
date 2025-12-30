f6ffb078f17a0efa2e5419e4d70d5d32d88d77b7
umbracious <63139410+umbracious@users.noreply.github.com>
2025-12-30 14:26:45 -0500

    fixed db update issue with cdn

--- genzineers/src/app/events/[eventID]/page.tsx
+++ genzineers/src/app/events/[eventID]/page.tsx
@@ -1,6 +1,8 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 import EventInfo from "@/components/eventInfo";
 
+export const revalidate = 0;
+
 export default async function EventDescriptionPage({params}: {params: {eventID: string}; }) {
   const event = await stripe.products.retrieve(params.eventID);
     return(
--- genzineers/src/app/events/page.tsx
+++ genzineers/src/app/events/page.tsx
@@ -1,5 +1,7 @@
 import EventsDisplayBox from "@/components/eventsDisplayBox";
 
+export const revalidate = 0;
+
 export default function EventPage() {
     return(
     <main className="md:pt-16 max-md:pt-24 flex flex-col items-center justify-end space-y-4">
